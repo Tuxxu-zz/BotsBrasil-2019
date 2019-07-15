adc508d21ab9258fb3fd235ffdf2043094b1ddb0
Tuxu <t.u.x.u@live.com>
2019-07-14 22:01:21 -0300

    add speaker

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
-});
 
 $(".nav-links a").click(function () {
   $("#nav-check").prop('checked', false);
@@ -23,3 +10,10 @@ $(".btn-close").click(function () {
   $("#nav-check").prop('checked', false);
   $(".btn-close").css({"display": "none"});
 });
+
+$(document).ready(function(){
+	$.getJSON('https://api.sheety.co/cb4073ac-9473-4af7-9921-9e38120ab1f0', function(data) {
+		var template = Handlebars.compile($('#list').html())
+		$('.wrap').html(template(data))
+	})
+})
